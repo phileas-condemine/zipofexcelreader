1c683bde25965af1cb4d192bfc17b2c412f5c496
Philéas Condemine <phileas.condemine@sante.gouv.fr>
2021-02-08 11:25:07 +0100

    first function - read excel files from a zip

---
+++ R/read_excel_in_zip.R
@@ -0,0 +1,23 @@
+
+#' Read The Excel Files In A Zip Archive
+#'
+#' @param zip_file path to a zip file that contains xls & xlsx files to be read
+#'
+#' @return a list of data.frame
+#' @export
+#' @import readxl
+#' @import purrr
+#' @importFrom magrittr %>%
+#' @importFrom utils unzip
+#'
+#' @examples
+#' read_excel_from_zip(system.file(package="zipofxlsxreader","datasets.zip"))
+read_excel_from_zip = function(zip_file){
+  td = tempdir()
+  unzip(zip_file,exdir = td,overwrite = TRUE)
+  files = list.files(td,recursive = TRUE,full.names = TRUE,pattern = "(\\.xlsx$)|(\\.xls$)")
+  poss_read_excel = possibly(read_excel,otherwise = NULL)
+
+  files%>%map(poss_read_excel)
+
+}
